c5d8f257970e015cdbe7321ce4ec9fb83af3fa05
aziztakci <takciazizz@gmail.com>
2026-03-09 21:32:54 +0300

    feat: implement structured data for multi-language support
    Defined complete "en" and "tr" datasets in data.js.
    
    Added detailed project descriptions and profile information.
    
    Set up placeholder TODOs for profile and project images.
    
    Structured sections for Hero, Skills, Profile, Projects, and Footer.

---
+++ src/data.js
@@ -0,0 +1,139 @@
+const data = {
+  en: {
+    heroSection: {
+      greeting: "Creative thinker",
+      intro: "Minimalism lover",
+      profileImage: "profile-image.jpg", //TODO
+      socials: [],
+      ctaHTML:
+        "Hi, I'm Almila. I'm a full-stack developer. If you are looking for a Developer who craft solid and scalable frontend products with great user experiences. Let's shake hands with me.",
+    },
+    skillsSection: {
+      title: "Skills",
+      skills: [
+        { name: "Java Script" },
+        { name: "React.Js" },
+        { name: "Node.Js" },
+      ],
+    },
+    profileSection: {
+      title: "Profile",
+      profile: [
+        { label: "Doğum tarihi", value: "24.03.1996" },
+        { label: "İkamet Şehri", value: "Ankara" },
+        {
+          label: "Eğitim Durumu",
+          value: "Hacettepe Ünv. Biyoloji, Lisans, 2016",
+        },
+        { label: "Tercih Ettiği Rol", value: "Frontend, UI" },
+      ],
+      aboutTitle: "About Me",
+      aboutText:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.\n\nMinima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
+    },
+    projectsSection: {
+      title: "Projects",
+      projects: [
+        {
+          name: "Workintech",
+          image: "project-1.jpg", //TODO
+          description:
+            "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          tags: ["react", "redux", "axios"],
+        },
+        {
+          name: "Random Jokes",
+          image: "project-2.jpg", //TODO
+          description:
+            "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          tags: ["react", "redux", "axios"],
+        },
+        {
+          name: "Journey",
+          image: "project-3.jpg", //TODO
+          description:
+            "A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline. This was created with vanilla JS, SCSS and Parcel Bundler and is available as a NPM package and the git repository makes any type of customization to code and themes possible.",
+          tags: ["react", "redux", "axios"],
+        },
+      ],
+    },
+    footerSection: {
+      title: "Let's work together on your next product.",
+      email: "almilasuccode@gmail.com",
+      links: [
+        { name: "Personal Blog" },
+        { name: "Github" },
+        { name: "Linkedin" },
+      ],
+    },
+  },
+
+  tr: {
+    heroSection: {
+      greeting: "Yaratıcı düşünür",
+      intro: "Minimalizm aşığı",
+      profileImage: "profile-image.jpg", //TODO
+      socials: [],
+      ctaHTML:
+        "Merhaba, ben Almila. Ben bir full-stack geliştiriciyim. Harika kullanıcı deneyimleri ile sağlam ve ölçeklenebilir frontend ürünleri geliştiren bir geliştirici arıyorsanız. Benimle el sıkışalım.",
+    },
+    skillsSection: {
+      title: "Yetenekler",
+      skills: [
+        { name: "Java Script" },
+        { name: "React.Js" },
+        { name: "Node.Js" },
+      ],
+    },
+    profileSection: {
+      title: "Profil",
+      profile: [
+        { label: "Doğum tarihi", value: "24.03.1996" },
+        { label: "İkamet Şehri", value: "Ankara" },
+        {
+          label: "Eğitim Durumu",
+          value: "Hacettepe Ünv. Biyoloji, Lisans, 2016",
+        },
+        { label: "Tercih Ettiği Rol", value: "Frontend, UI" },
+      ],
+      aboutTitle: "Hakkımda",
+      aboutText:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam aut, odit laborum aliquam voluptatum nisi mollitia.\n\nMinima accusamus ratione soluta aperiam sit voluptate? Dicta quod deserunt quam temporibus cumque magnam!",
+    },
+    projectsSection: {
+      title: "Projeler",
+      projects: [
+        {
+          name: "Workintech",
+          image: "project-1.jpg", //TODO
+          description:
+            "Kullanıcıların hangi çerezleri kabul edip reddedeceğini seçmesine izin veren basit, özelleştirilebilir, minimal kurulumlu bir çerez eklentisi. Vanilla JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur, ayrıca git deposu kod ve tema üzerinde her türlü özelleştirmeyi mümkün kılar.",
+          tags: ["react", "redux", "axios"],
+        },
+        {
+          name: "Random Jokes",
+          image: "project-2.jpg", //TODO
+          description:
+            "Kullanıcıların hangi çerezleri kabul edip reddedeceğini seçmesine izin veren basit, özelleştirilebilir, minimal kurulumlu bir çerez eklentisi. Vanilla JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur, ayrıca git deposu kod ve tema üzerinde her türlü özelleştirmeyi mümkün kılar.",
+          tags: ["react", "redux", "axios"],
+        },
+        {
+          name: "Journey",
+          image: "project-3.jpg", //TODO
+          description:
+            "Kullanıcıların hangi çerezleri kabul edip reddedeceğini seçmesine izin veren basit, özelleştirilebilir, minimal kurulumlu bir çerez eklentisi. Vanilla JS, SCSS ve Parcel Bundler ile oluşturulmuştur ve bir NPM paketi olarak mevcuttur, ayrıca git deposu kod ve tema üzerinde her türlü özelleştirmeyi mümkün kılar.",
+          tags: ["react", "redux", "axios"],
+        },
+      ],
+    },
+    footerSection: {
+      title: "Bir sonraki ürününüz üzerinde birlikte çalışalım.",
+      email: "almilasuccode@gmail.com",
+      links: [
+        { name: "Kişisel Blog" },
+        { name: "Github" },
+        { name: "Linkedin" },
+      ],
+    },
+  },
+};
